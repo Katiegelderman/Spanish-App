74ae3c004d0ea33bd70a11d7ceb07065c5a20eb2
Katie Gelderman <148262796+Katiegelderman@users.noreply.github.com>
2026-06-30 21:13:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,34 @@
+const FILES_TO_CACHE = [
+  "/LOTG/",
+  "/LOTG/index.html",
+  "/LOTG/manifest.json",
+  "/LOTG/sw.js"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("spanish-trainer-v1").then(cache => {
-      return cache.addAll(["./"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
